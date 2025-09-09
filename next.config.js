39794e123bcbe1facb5bb9e2df6f4ca74cf5b71b
unforgettablekartik <unforgettablekartik@gmail.com>
2025-09-09 21:49:49 +0530

    Add root layout and redirect for blog

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/articles',
+        destination: '/blog',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
