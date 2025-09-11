ed0a25783c8e7f0eb7d372be8f8be81f18afcd92
unforgettablekartik <unforgettablekartik@gmail.com>
2025-09-11 16:04:16 +0530

    style: update footer text and color

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer style={{ textAlign: 'center', padding: '1rem 0', background: '#f1f1f1' }}>
-      <p>&copy; {new Date().getFullYear()} Compilers App. All rights reserved.</p>
+    <footer
+      style={{ textAlign: 'center', padding: '1rem 0', background: '#f1f1f1', color: '#000' }}
+    >
+      <p>The Compilers © 2025. All rights reserved.</p>
     </footer>
   );
 }
