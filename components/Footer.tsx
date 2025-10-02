51409b33ae52c3c0866e8116dbd6ecb56af6f690
unforgettablekartik <unforgettablekartik@gmail.com>
2025-10-02 16:49:45 +0530

    Update footer copyright text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer style={{ textAlign: 'center', padding: '1rem 0', background: '#f1f1f1' }}>
-      <p>&copy; {new Date().getFullYear()} Compilers App. All rights reserved.</p>
+      <p style={{ color: '#000' }}>The Compliers © 2025 | All Rights Reserved</p>
     </footer>
   );
 }
